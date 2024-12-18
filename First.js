e8f0218832f3bee89d1bc7a67d67b99b3f35d41a
rushikesh3393 <rushikeshmadake3393@gmail.com>
2024-12-18 21:12:55 +0530

    Javascript practice code deployed

---
+++ First.js
@@ -0,0 +1,22 @@
+
+
+/*var a=1000;
+document.write(typeof(a)+"<br>");
+
+a=rushi;
+document.write(typeof(a)+"br");
+
+a=5.5;
+document.write(typeof(a)+"<br>");*/
+
+/*let a=10;
+let b=20;
+let c=a+b;
+
+document.write("Addition is:"+c);*/
+
+const a=100;
+
+
+document.write("The value of A is:"+a);
+
